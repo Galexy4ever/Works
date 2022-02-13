603db36281440bed719f79f1083f2186e68437fb
Alexey <mctasb@yandex.ru>
2022-02-13 14:44:47 +0300

    Change API

--- Shop/js/main.js
+++ Shop/js/main.js
@@ -1,4 +1,4 @@
-const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+const API = 'https://raw.githubusercontent.com/Galexy4ever/Works/master/Shop';
 
 const app = new Vue({
     el: '#app',
